999bfeccbd8bfada343e4c1f02ae210f0e1e9753
Ayyanar M <ayyanar.madasamy@volantetech.com>
2021-06-19 10:09:38 +0530

    Type script Amd Example has been added

---
+++ ts-amd-example/src/App.ts
@@ -0,0 +1,4 @@
+import { Class1 } from "./Class1";
+console.log("executing the class");
+var classObj = new Class1("ayyanar");
+console.log(`My Name is ${classObj.getName()}`)
---
+++ ts-amd-example/src/Class1.ts
@@ -0,0 +1,10 @@
+export class Class1{
+     name: string;
+    constructor(name: string){
+        this.name = name;
+    }
+
+    getName() : string{
+        return this.name;
+    }
+}
